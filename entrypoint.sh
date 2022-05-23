6fe589e08ec9980269d0a09ef00a281f64bfec1f
Alex Diachenko <alex@superblockshq.com>
2022-05-23 16:35:18 -0400

    Missing double quotes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,7 +68,7 @@ case "$tag_context" in
         tag="$(semver $taglist | tail -n 1)"
 
         pre_taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt")"
-        pre_tag=$(semver "$pre_taglist" | tail -n 1)
+        pre_tag="$(semver "$pre_taglist" | tail -n 1)"
         ;;
     * ) echo "Unrecognised context"; exit 1;;
 esac
